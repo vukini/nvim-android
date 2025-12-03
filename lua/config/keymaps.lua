ef3227a6d905b1b0e1d0b462d6223927da995d11
Vikid <vukini@gmail.com>
2025-12-03 10:36:23 +0400

    leader tn and tp for buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,5 @@
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+vim.keymap.set('n', '<Leader>tp', '<cmd>bprevious<cr>', { desc = "Buffer Previous"}) 
+vim.keymap.set('n', '<Leader>tn', '<cmd>bnext<cr>', { desc = "Buffer Next"}) 
