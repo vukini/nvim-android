ad2b28ba675574500161d1dfaad504cbe9d46bfc
Vikid <vukini@gmail.com>
2025-12-03 07:14:59 +0400

    took options into options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,10 @@
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
+vim.g.netrw_banner = 1 
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4 -- " open files in previous window
+vim.g.netrw_winsize = 25   --   " explorer width
+
 
