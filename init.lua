d2032d6fd2002358327cb92e65b24fefa84f5734
Vikid <vukini@gmail.com>
2025-12-03 07:10:48 +0400

    Created Keymaps, editednit out of init

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
 vim.g.netrw_banner = 1 
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 4 -- " open files in previous window
@@ -9,11 +10,10 @@ vim.g.netrw_winsize = 25   --   " explorer width
 require("config.lazy")
 require("config.options-noice")
 require("config.options-lualine")
+require("config.keymaps")
+require("bufferline").setup{}
 
-vim.opt.termguicolors = true
 
- require("bufferline").setup{}
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 
