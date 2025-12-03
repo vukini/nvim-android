5b09025ef52263c7ec3ff9f15ccd48c294186152
Vikid <vukini@gmail.com>
2025-12-03 14:29:13 +0400

    added mason-lspconfig to mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,16 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+	{
+    	  "mason-org/mason.nvim",
+    	  opts = {}
+  	},
+	{
+    	"mason-org/mason-lspconfig.nvim",
+    	opts = {
+        	ensure_installed = { "pyright", },
+    		},
+ 	   	dependencies = {
+        	{ "mason-org/mason.nvim", opts = {} },
+        	"neovim/nvim-lspconfig",
+    		},
+	}
 }
